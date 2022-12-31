b4f82b40c83fd2538cf1a6099cca31bc8fbe3d4a
DavidTran <dtrancpp@gmail.com>
2022-12-30 21:41:56 -0800

    add Button.js, add Button.css, added image to HomePage.js

--- c2hd/src/App.js
+++ c2hd/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
-// import HomePage from "./HomePage";
+import HomePage from "./components/HomePage.js";
+import Button from "./components/Button";
+import React, { useState, useEffect } from "react";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Nav />
+			<HomePage />
 			<div className="container">
 				<Routes>{/* <Route path="/" element={<HomePage />} /> */}</Routes>
 			</div>
--- c2hd/src/components/HomePage.js
+++ c2hd/src/components/HomePage.js
@@ -1,2 +1,15 @@
 import "./HomePage.css";
-import React from "react";
+// import React from "react";
+import Button from "./Button.js";
+import React, { useState, useEffect } from "react";
+import bballCourtImage from "../images/basketball-court.jpg";
+
+function HomePage() {
+	return (
+		<div className="homepage-container">
+			<img className="bballCourtImg" src={bballCourtImage} alt="bballCourt" />
+		</div>
+	);
+}
+
+export default HomePage;
