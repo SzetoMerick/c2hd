ebca81bf89ce334fcf3d07114ba6d2f4a31ff018
DavidTran <dtrancpp@gmail.com>
2022-12-31 19:32:26 -0800

    add /node_modules back to gitignore, create working buttons for changing images in HomePage.js, and made corresponding HomePage.css changes

--- c2hd/src/components/HomePage.js
+++ c2hd/src/components/HomePage.js
@@ -1,13 +1,47 @@
 import "./HomePage.css";
 // import React from "react";
-import Button from "./Button.js";
+import Button from "@mui/material/Button";
 import React, { useState, useEffect } from "react";
 import bballCourtImage from "../images/basketball-court.jpg";
+import toiletImage from "../images/toilet.jpg";
+import gazeboImage from "../images/gazebo.jpg";
 
 function HomePage() {
+	const [image, setImage] = useState("image1.jpg");
 	return (
-		<div className="homepage-container">
-			<img className="bballCourtImg" src={bballCourtImage} alt="bballCourt" />
+		<div className="homepage_container">
+			<div className="img_container">
+				<img className="homepage_img" src={image} alt="Image" />
+			</div>
+			<div className="image_button_container">
+				<li>
+					<Button
+						className="bball_button"
+						variant="contained"
+						onClick={() => setImage(bballCourtImage)}
+					>
+						Basketball Court Installation
+					</Button>
+				</li>
+				<li>
+					<Button
+						className="toilet_button"
+						variant="contained"
+						onClick={() => setImage(toiletImage)}
+					>
+						Toilet Replacement
+					</Button>
+				</li>
+				<li>
+					<Button
+						className="gazebo_button"
+						variant="contained"
+						onClick={() => setImage(gazeboImage)}
+					>
+						Gazebo Installation
+					</Button>
+				</li>
+			</div>
 		</div>
 	);
 }
