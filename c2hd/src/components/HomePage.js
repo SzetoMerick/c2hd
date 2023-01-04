eed9ac01f45b7b66b7977a75e7a316d4fc5581ff
DavidTran <dtrancpp@gmail.com>
2023-01-03 20:13:15 -0800

    added mui icon library, made HomePage.js and HomePage.css changes

--- c2hd/src/components/HomePage.js
+++ c2hd/src/components/HomePage.js
@@ -7,61 +7,66 @@ import React, { useState, useEffect } from "react";
 import bballCourtImage from "../images/basketball-court.jpg";
 import toiletImage from "../images/toilet.jpg";
 import gazeboImage from "../images/gazebo.jpg";
+import SportsBasketballIcon from "@mui/icons-material/SportsBasketball";
+import RoofingIcon from "@mui/icons-material/Roofing";
+import WcIcon from "@mui/icons-material/Wc";
 
 function HomePage() {
-  const [image, setImage] = useState(bballCourtImage);
-  const [description, setDescription] = useState("");
+	const [image, setImage] = useState(bballCourtImage);
+	const [description, setDescription] = useState("");
 
-  return (
-    <div className="homepage_container">
-      <div className="img_container">
-        <img className="homepage_img" src={image} alt="Image" />
-      </div>
-      <div className="image_button_container">
-        <li>
-          <Button
-            className="bball_button"
-            variant="contained"
-            onClick={() => {
-              setImage(bballCourtImage);
-              setDescription("Basketball Court Installation");
-            }}
-          >
-            Basketball Court Installation
-          </Button>
-        </li>
-        <li>
-          <Button
-            className="toilet_button"
-            variant="contained"
-            onClick={() => {
-              setImage(toiletImage);
-              setDescription("Toilet Replacement");
-            }}
-          >
-            Toilet Replacement
-          </Button>
-        </li>
-        <li>
-          <Button
-            className="gazebo_button"
-            variant="contained"
-            onClick={() => {
-              setImage(gazeboImage);
-              setDescription("Gazebo Installation");
-            }}
-          >
-            Gazebo Installation
-          </Button>
-        </li>
-      </div>
-      {description ? (
-        <div className="description_container">{description}</div>
-      ) : (
-        <div></div>
-      )}
-    </div>
-  );
+	return (
+		<div className="homepage_container">
+			<div className="img_container">
+				<img className="homepage_img" src={image} alt="Image" />
+			</div>
+			<div className="image_button_container">
+				<li>
+					<Button
+						className="bball_button"
+						variant="contained"
+						startIcon={<SportsBasketballIcon />}
+						endIcon={<SportsBasketballIcon />}
+						onClick={() => {
+							setImage(bballCourtImage);
+							setDescription("Basketball Court Installation");
+						}}
+					>
+						Basketball Court Installation
+					</Button>
+				</li>
+				<li>
+					<Button
+						className="toilet_button"
+						variant="contained"
+						startIcon={<WcIcon />}
+						endIcon={<WcIcon />}
+						onClick={() => {
+							setImage(toiletImage);
+							setDescription("Toilet Replacement");
+						}}
+					>
+						Toilet Replacement
+					</Button>
+				</li>
+				<li>
+					<Button
+						className="gazebo_button"
+						variant="contained"
+						startIcon={<RoofingIcon />}
+						endIcon={<RoofingIcon />}
+						onClick={() => {
+							setImage(gazeboImage);
+							setDescription("Gazebo Installation");
+						}}
+					>
+						Gazebo Installation
+					</Button>
+				</li>
+			</div>
+			{description ? <div className="description_container">{description}</div> : <div></div>}
+		</div>
+	);
 }
 
 export default HomePage;
