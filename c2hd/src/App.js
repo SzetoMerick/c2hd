94ccb9a738369040d401bf1cb7dec4d1b7d5e748
SzetoMerick <110795519+SzetoMerick@users.noreply.github.com>
2023-01-03 20:06:43 -0800

    add contact form

--- c2hd/src/App.js
+++ c2hd/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import HomePage from "./components/HomePage.js";
+import Contact from "./components/contact";
 import Footer from "./components/Footer.js";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Nav />
-			<HomePage />
+
 			<div className="container">
-				{/* <Routes><Route path="/" element={<HomePage />} /></Routes> */}
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
 			</div>
 			<Footer />
 		</BrowserRouter>
