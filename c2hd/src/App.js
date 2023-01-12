65d516bedf13c23ec37c642b9d26c30296b286bc
SzetoMerick <110795519+SzetoMerick@users.noreply.github.com>
2023-01-12 00:03:10 -0800

    added a employee section

--- c2hd/src/App.js
+++ c2hd/src/App.js
@@ -5,21 +5,24 @@ import Contact from "./components/ContactForm/contact";
 import Footer from "./components/FooterMenu/Footer.js";
 import YelpReviews from "./components/YelpReviews.js";
 import TestiMonials from "./components/Yelp/Testimonials/t.js"
+import Header from "./components/Owners/owners.jsx"
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Nav />
-			<YelpReviews />
+			{/* <YelpReviews /> */}
 			<HomePage />
 			<Contact />
 			<TestiMonials />
+			<Header/>
 
 
 			<div className="container">
 				<Routes>
 					<Route path="/contact" element={<Contact />} />
 					<Route path="/testimonials" element={<TestiMonials />} />
+					<Route path="/owners" element={<Header />} />
 				</Routes>
 			</div>
 			<Footer />
---
+++ c2hd/src/components/Owners/owners.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "./owners.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className = "container">
+                <div className = "row">
+                    <div className = "col-6">
+                        <div className="header__content">
+                            <div className = "header-section">
+                                <h2>Hello, I am</h2>
+                                <h1>Rick Szeto</h1>
+                                <p>Established in 1980.
+
+My dad started this business when after we landed on American soil back in 1980. We didn’t know what the American dream was, but my dad just taught us from the beginning to be honest and reliable. He believe if you treat your customer right, your customer will reward with loyalty and respect. That’s how this company got its start and now I am honored and privileged to carry on the legacy and traditions.</p>
+                                <div className="header__buttons">
+                                    <a href="header" className = "btn btn-outline">
+                                        Hire me
+                                    </a>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                    <div className="col-6">
+                        <img src="../merick.jpg" alt="" className = "image" />
+
+
+                    </div>
+
+                </div>
+            </div>
+
+
+        </header>
+    )
+}
+
+export default Header
